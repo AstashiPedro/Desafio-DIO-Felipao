3c0086622725904739a7d689702c183e50fa5dc2
Pedro Augusto Barbosa Pereira <peteraugustobp@gmail.com>
2024-07-16 22:05:26 -0300

    erro corrigo de loop

--- estrutura.js
+++ estrutura.js
@@ -27,4 +27,23 @@ while (XP >= 0) {
     for(XP <= 0; XP <= 10000; XP += 100){
         console.log("Upgrade...." + XP)
       }
+    }
+    while (XP >= 0) {
+        if (XP >= 0 && XP < 1000) {
+            nivel = "Ferro"
+        } else if (XP >= 1000 && XP <= 2000) {
+            nivel = "Bronze"
+        } else if (XP >= 2000 && XP <= 5000) {
+            nivel = "Prata"
+        } else if (XP >= 5000 && XP <= 7000) {
+            nivel = "Ouro"
+        } else if (XP >= 7000 && XP <= 8000) {
+            nivel = "Platina"
+        } else if (XP >= 8000 && XP <= 9000) {
+            nivel = "Ascendente"
+        } else if (XP >= 9000 && XP <= 10000) {
+            nivel = "Imortal"
+        } else if (XP >= 10000) {
+            nivel = "Radiante"
+        }
 }
